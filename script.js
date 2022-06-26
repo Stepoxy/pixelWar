9a6c170dabac3fdb6243e961353d5c8b9a35b2ff
Stepoxy <stepoxy.cartelet@gmail.com>
2022-06-26 13:01:55 +0200

    Première partie de la formation terminée

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
+const colorsChoice = document.querySelector('#colorsChoice');
 const game = document.querySelector('#game');
 game.width = 1200
 game.height = 600
 
 const ctx = game.getContext('2d');
 
-ctx.beginPath();
-ctx.fillStyle = 'red';
-ctx.fillRect(250, 50, 50, 50);
+const colorList = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF', '#8B00FF'];
+let currentColorChoice = colorList[6];
+
+colorList.forEach(color => {
+    const colorItem = document.createElement('div');
+    colorItem.style.backgroundColor = color;
+    colorsChoice.appendChild(colorItem);
+
+    colorItem.addEventListener('click', () => {
+        currentColorChoice = color
+
+        colorItem.innerHTML = `<i class="fa-solid fa-check"></i>`
+
+        setTimeout(() => {
+            colorItem.innerHTML = ''
+        },1000)
+    })
+})
+
+game.addEventListener('click', (e) => {
+    const x = e.clientX
+    const y = e.clientY - game.offsetTop
+    
+    ctx.beginPath();
+    ctx.fillStyle = currentColorChoice;
+    ctx.fillRect(x, y, 50, 50);
+})
