231aadf7e1f83be17f8cb275f3de23bdb6f890b2
Stepoxy <stepoxy.cartelet@gmail.com>
2022-06-27 12:34:53 +0200

    Deuxième partie de la formation terminée

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const colorsChoice = document.querySelector('#colorsChoice');
 const game = document.querySelector('#game');
+const cursor = document.querySelector('#cursor');
 game.width = 1200
 game.height = 600
+const gridCellSize = 10
 
 const ctx = game.getContext('2d');
+const gridCtx = game.getContext('2d');
 
 const colorList = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF', '#8B00FF'];
 let currentColorChoice = colorList[6];
@@ -24,11 +27,51 @@ colorList.forEach(color => {
     })
 })
 
-game.addEventListener('click', (e) => {
-    const x = e.clientX
-    const y = e.clientY - game.offsetTop
-    
+function addPixelIntoGame(){
+
+        const x = cursor.offsetLeft
+        const y = cursor.offsetTop - game.offsetTop
+
+        console.log("click");
+        
+        ctx.beginPath();
+        ctx.fillStyle = currentColorChoice;
+        ctx.fillRect(x, y, gridCellSize, gridCellSize);
+}
+cursor.addEventListener('click', (e) =>{
+    addPixelIntoGame()
+})
+game.addEventListener('click', () =>{
+    addPixelIntoGame()
+})
+
+
+
+
+function drawGrids(ctx, width, height, cellWidth, cellHeight) {
+
     ctx.beginPath();
-    ctx.fillStyle = currentColorChoice;
-    ctx.fillRect(x, y, 50, 50);
+    ctx.strokeStyle = '#ccc';
+
+    for(let i = 0; i < width; i++){
+        ctx.moveTo(i * cellWidth, 0);
+        ctx.lineTo(i * cellWidth, height);
+    }
+    for(let i = 0; i < height; i++){
+        ctx.moveTo(0, i * cellHeight);
+        ctx.lineTo(width, i * cellHeight);
+    }
+    ctx.stroke()
+}
+drawGrids(gridCtx, game.width, game.height, gridCellSize, gridCellSize);
+
+game.addEventListener('mousemove', (e) =>{
+    console.log('x :', e.clientX);
+    console.log('y :', e.clientY);
+
+    const cursorLeft = e.clientX - (cursor.offsetWidth / 2);
+    const cursorTop = e.clientY - (cursor.offsetHeight / 2);
+
+    cursor.style.left = `${Math.floor(cursorLeft / gridCellSize) * gridCellSize}px`;
+    cursor.style.top = `${Math.floor(cursorTop / gridCellSize) * gridCellSize}px`;
 })
